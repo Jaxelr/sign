81703857db8e27ca245248ae0bdbb7497e5dadb2
Jaxel Rojas Lopez <jrojaslopez@microsoft.com>
2024-05-24 13:37:21 -0400

    chore: scaffolding classes to fulfill

---
+++ src/Sign.Cli/TrustedSigningCommand.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+using Sign.Core;
+using System.CommandLine;
+
+namespace Sign.Cli
+{
+    internal sealed class TrustedSigningCommand : Command
+    {
+        internal TrustedSigningCommand(CodeCommand codeCommand, IServiceProviderFactory serviceProviderFactory)
+            : base("trusted-signing", AzureKeyVaultResources.CommandDescription)
+        {
+        }
+    }
---
+++ src/Sign.Core/CertificateServices/TrustedSigningService.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE.txt file in the project root for more information.
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Sign.Core.CertificateServices
+{
+    internal sealed class TrustedSigningService : ISignatureAlgorithmProvider, ICertificateProvider
+    {
+        public Task<X509Certificate2> GetCertificateAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RSA> GetRsaAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
